65368188570056c66c6ca2db38c9eedddbc063f5
webmania2014 <panda1987417@hotmail.com>
2017-02-07 05:06:41 +0100

    add blog index page

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,11 @@ use App\Post;
 
 class BlogController extends Controller
 {
+    public function getIndex() {
+      $post = Post::paginate(1);
+      return view('blog.index')->withPosts($post);
+    }
+
     public function getSingle($slug) {
       // fetch from the DB based on slug
       $post = Post::where('slug', '=', $slug)->get()->first();
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
+Route::get('blog', ['as' => 'blog.index', 'uses' => 'BlogController@getIndex']);
 Route::get('/', 'PagesController@getIndex');
 Route::get('about', 'PagesController@getAbout');
 Route::get('contact', 'PagesController@getContact');
