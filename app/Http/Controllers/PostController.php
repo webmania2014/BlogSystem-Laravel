ef20edfcbfc56e33bc6da5d296fc249c2ce5b9a1
webmania2014 <panda1987417@hotmail.com>
2017-02-12 15:25:05 +0100

    add security feature for CKEditor

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,7 @@ use App\Post;
 use App\Category;
 use App\Tag;
 use Session;
+use Purifier;
 
 class PostController extends Controller
 {
@@ -57,7 +58,7 @@ class PostController extends Controller
         $post->title = $request->title;
         $post->slug = $request->slug;
         $post->category_id = $request->category_id;
-        $post->body = $request->body;
+        $post->body = Purifier::clean($request->body);
 
         $post->save();
 
@@ -144,7 +145,7 @@ class PostController extends Controller
         $post->title = $request->input('title');
         $post->slug = $request->input('slug');
         $post->category_id = $request->input('category_id');
-        $post->body = $request->input('body');
+        $post->body = Purifier::clean($request->input('body'));
         // $post->title = $request->title;
         // $post->body = $request->body;
 
